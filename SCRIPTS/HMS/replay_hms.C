29c7d0de92906252a483a7201ef1a1df1bdde8e3
Carlos Yero <cyero002@fiu.edu>
2018-07-02 15:51:49 -0400

    modified general dc plane time offsets, added flag to replay dc_calib on replay script, added hod_Track variables to DEF-files/dc_calib.def

--- SCRIPTS/HMS/replay_hms.C
+++ SCRIPTS/HMS/replay_hms.C
@@ -1,4 +1,4 @@
-void replay_hms(Int_t RunNumber=0, Int_t MaxEvent=0,const char* ftype="matrixopt") {
+void replay_hms(Int_t RunNumber=0, Int_t MaxEvent=0,const char* ftype="dc_calib") {
 
   // Get RunNumber and MaxEvent if not provided.
   if(RunNumber == 0) {
